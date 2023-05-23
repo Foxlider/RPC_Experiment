ab493e93b51f86a5ded4faa77e64e1fd2394faa2
johan <dev@johan.moe>
2023-05-23 14:27:07 +0000

    feat: added cli

--- Client_1/bin/build.rs
+++ Client_1/bin/build.rs
@@ -1,3 +1,16 @@
+use clap::CommandFactory;
+
+#[path = "src/cli.rs"]
+mod cli;
+
 fn main() {
     tonic_build::compile_protos("../../protos/hello.proto").unwrap();
+
+    let out_dir = std::path::PathBuf::from(std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound).unwrap());
+    let target_dir = out_dir.parent().unwrap().parent().unwrap().parent().unwrap();
+    let man = clap_mangen::Man::new(cli::App::command());
+    let mut buffer: Vec<u8> = Default::default();
+    man.render(&mut buffer).unwrap();
+
+    std::fs::write(target_dir.join("client_1.1"), buffer).unwrap();
 }
--- Client_1/bin/src/main.rs
+++ Client_1/bin/src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use hello::{say_client::SayClient, SayRequest};
 use std::env::var;
 
@@ -5,18 +6,21 @@ pub mod hello {
     tonic::include_proto!("hello");
 }
 
+mod cli;
+
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     color_eyre::install()?;
+    cli::App::parse();
 
-    let mut client =
-        SayClient::connect(var("GRPC_HOST").unwrap_or_else(|_| "http://0.0.0.0:50051".to_string())).await?;
+    let mut manager_client =
+        SayClient::connect(var("GRPC_MANAGER_HOST").unwrap_or_else(|_| "http://0.0.0.0:50051".to_string())).await?;
 
     let request = tonic::Request::new(SayRequest {
         name: "PiNg".to_string()
     });
 
-    let response = client.send(request).await?;
+    let response = manager_client.send(request).await?;
 
     println!("RESPONSE={:?}", response);
 
